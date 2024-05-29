88cc21d3a02dcc536a5c33316f1eaf2726fc940d
siamdev1 <120947031+siamdev1@users.noreply.github.com>
2024-05-29 20:28:26 +0600

    update the js

--- js/script.js
+++ js/script.js
@@ -62,15 +62,4 @@ document.addEventListener('DOMContentLoaded', function() {
         priceSpan1.textContent = `$${roomCount1 * pricePerRoom1}`;
     }
 
-    const contactForm = document.getElementById('contact-form');
-    contactForm.addEventListener('submit', function(event) {
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-
-        if (!name || !email || !message) {
-            event.preventDefault();
-            alert('Please fill in all fields.');
-        }
-    });
 });
